86fd0e11a7dc89062501515ce8600d8f850d67e7
Adebesin Aramide <adebesinaramide@gmail.com>
2023-12-27 13:14:59 +0000

    commit -m "added images to portfolio"

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,33 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/pexels-timur-saglambilek-185576.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "data analytics on a screen";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mathematics Student studying at Obafemi Awolowo University. I am a Machine Learning Engineer that is passionate about creating solutions.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "Machine Learning",
+  "PyTorch",
+  "Natural Language Processing",
+  "Computer Vision",
+  "Data Science",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Machine Learning skills I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/pexels-lukas-669619.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Data analytics on a paper";
 
 /**
  * Project list
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Open Science Chatbot",
+    description: "This a chatbot that helps researchers find open science tools and resources.",
+    url: "https://github.com/Adebesin-Aramide/Open-Science-Chatbot",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Heart Disease Prediction",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is a machine learning model that predicts the presence of heart disease in a patient.",
+    url: "https://github.com/Adebesin-Aramide/Heart_Disease_Prediction",
   },
   {
-    title: "My Resume Site",
+    title: "Global Household Electrification",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is a machine learning model that predicts if a household has electricity or not.",
+    url: "https://github.com/Adebesin-Aramide/Global_Household_Electrification",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My Datathon Experience",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is an article I wrote about my experience at the 2023 DataFest Africa Datathon.",
+    url: "https://medium.com/@adebesinaramide/my-datathon-experience-e4cbe5a3ab2c",
   },
 ];
 
